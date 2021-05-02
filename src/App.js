585bfab074e8fa8e33a3585b0ca81d2645863fb8
girkarkshitij <girkarkshitij@gmail.com>
2021-05-02 12:44:58 +0530

    Initial cleanup; start basic html structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='container'>
+      <h1>Pomodoro Timer</h1>
+      <div>
+        <h3 id='break-label'>Break Length</h3>
+        <button id='break-decrement'>-</button>
+        <h3 id='break-length'>5</h3>
+        <button id='break-increment'>+</button>
+        <h3 id='session-label'>Session Length</h3>
+        <button id='session-decrement'>-</button>
+        <h3 id='session-length'>25</h3>
+        <button id='session-increment'>+</button>
+      </div>
+      <div>
+        <h3 id='timer-label'>Session</h3>
+        <h3 id='time-left'>25:00</h3>
+      </div>
+      <div>
+        <button id='start_stop'>Start Stop</button>
+        <button id='reset'>Reset</button>
+      </div>
+      <footer>Built by Kshitij Girkar</footer>
     </div>
   );
-}
+};
 
 export default App;
